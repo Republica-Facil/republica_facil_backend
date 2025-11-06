e51de854ffc420e6d2f770037e299815d59d2f9f
Rafael Melo Matuda <rafael.matuda10@gmail.com>
2025-11-06 13:20:17 -0300

    fix: Reorder import statements for consistency and clarity

--- republica_facil/main.py
+++ republica_facil/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from republica_facil.settings import Settings
 from republica_facil.autenticacao import router as auth
 from republica_facil.membros import router as membros
 from republica_facil.republicas import router as republicas
+from republica_facil.settings import Settings
 from republica_facil.usuarios import router as user
 
 app = FastAPI()
@@ -17,8 +17,8 @@ app.add_middleware(
         Settings().LOCALHOST_FRONTEND_ADDRESS,
     ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(auth.router)
